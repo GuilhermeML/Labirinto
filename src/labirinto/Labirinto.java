7a2dc0112626c1df75ca9ab258df44f0384a9173
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2018-07-07 17:15:24 -0300

    Segundo Commit

--- src/labirinto/Labirinto.java
+++ src/labirinto/Labirinto.java
@@ -16,6 +16,7 @@ public class Labirinto {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello UFABC!");
         // TODO code application logic here
     }
     
